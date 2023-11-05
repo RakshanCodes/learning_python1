887e00d198718305008a9b57f75a24b558bc8664
RakshanCodes <88967427+RakshanCodes@users.noreply.github.com>
2023-11-05 14:03:27 +0530

    Hi sir. There is one small error:TypeError: Bikes._init_() missing 2 required positional arguments: 'cost' and 'items'

--- vehicles.py
+++ vehicles.py
@@ -1,1 +1,35 @@
-print("hi")
+class Bikes:
+    def _init_(self,name,color,cost):
+        print("_init_",name)
+        self.name = name
+        self.cost = cost
+        self.color= color
+    def cost(self):
+        return self.cost
+    def name(self):
+        return self.name
+    def color(self):
+        return self.color
+class highness(Bikes):
+    def __init__(self, name, color, cost):
+        super(highness,self)._init_(
+            f"{name} Honda highness",cost,color ["Honda"])
+class BMW(Bikes):
+    def __init__(self, name, color, cost):
+        super(BMW,self)._init_(
+            f"{name} BMW",cost,color ["BMW"])
+        
+        
+    
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
